6b7888f923fb352873871a6ff527ca435573bfc3
DhruvVerseX <dhruvnegi2040@gmail.com>
2025-03-17 00:28:46 +0530

    Integrate Tailwind CSS into the project by updating package.json, vite.config.js, and CSS files. Remove unused styles from App.css and index.css, and implement a custom font in App.css. Refactor App.jsx to utilize a Header component and apply Tailwind CSS classes for layout.

---
+++ expanse-tracker/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <header className='bg-amber-100 h-screen flex justify-center items-center '>
+         <div className='font-bold text-3xl font-family-MyCustomFont'>
+        <h2>Expense Tracker</h2>
+    </div>
+    </header>
+   
+  )
+}
