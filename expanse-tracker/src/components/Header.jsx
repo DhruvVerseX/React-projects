95ac55b9217e1f1acfaf51d5c90a2bb2a7a5aaf7
DhruvVerseX <dhruvnegi2040@gmail.com>
2025-03-17 23:33:26 +0530

    Update package dependencies and integrate Tailwind CSS utilities. Refactor App.css to correct font path and include Tailwind's preflight styles. Modify Header component to use custom font and adjust class names. Remove obsolete font file.

--- expanse-tracker/src/components/Header.jsx
+++ expanse-tracker/src/components/Header.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export const Header = () => {
   return (
     <header className='bg-amber-100 h-screen flex justify-center items-center '>
-         <div className='font-bold text-3xl font-family-MyCustomFont'>
+         <div className='font-coustom text-3xl '>
+         <h1 class="font-custom text-4xl">Hello with MyCustomFont!</h1>
         <h2>Expense Tracker</h2>
     </div>
     </header>
