f883ef79c48402fe63edb7acf13fc4c575c2f677
DhruvVerseX <dhruvnegi2040@gmail.com>
2025-03-24 18:13:33 +0530

    Update package.json to include react-icons dependency, modify pnpm-lock.yaml for version resolution, and refactor App component layout for improved structure and styling.

--- expanse-tracker/src/App.jsx
+++ expanse-tracker/src/App.jsx
@@ -3,33 +3,46 @@ import './App.css';
 
 function App() {
   return (
-    <div className="h-screen w-28 bg-slate-500 flex flex-col ml-50% items-center justify-center">
-      <div className='item-start'>
-        <h1 className="text-3xl  font-cassandra flex items-center ">Expanse Tracker</h1>
-      <div className=''>
-        <h3 className="uppercase font-cassandra">Your Balance</h3>
-        <p>$260.00</p>
-      </div>
-      <div className='h-12 w-fit bg-slate-400 gap-6  border rounded-lg flex flex-grow  items-center justify-center'>
-        <div className='space-x-4'>
-          <h2>
-            INCOME
-          </h2>
-          <span>
-            500$
-          </span>
+    <div className='w-full h-screen flex flex-col items-center justify-center'>
+      <h1>Expense Tracker</h1>
+      <div className='w-full '>
+        <h3 className=''>
+          YOUR BALANCE
+        </h3>
+        <span className=''>$260.00</span>
+        <div className='flex justify-center items-center border '>
+          <div className='border'>
+            <h2>INCOME</h2>
+            <span>500$</span>
+          </div>
+          <div className='border'>
+            <h2>EXPENSE</h2>
+            <span>240$</span>
+          </div>
+          
         </div>
+      </div>
+      <div>
+        <h3>
+          History
+        </h3>
         <div>
-          <h2>
-            EXPANSE
-          </h2>
-          <span>
-            200$
-          </span>
+          cash
         </div>
+        <div>Book</div>
+        <div>Camera</div>
       </div>
+      <div className='flex flex-col items-center justify-center'>
+        <h3>Add new Tracnsaction</h3>
+        <h4>Text</h4>
+        <input type="text" placeholder='Enter text...'/>
+        <h4>Amount</h4>
+        <span>
+          (negative-expense, positive-expense)
+        </span>
+        <input type="number" placeholder='Enter amount...'/>
       </div>
-      
+      <button>Add transaction</button>
     </div>
   );
 }
